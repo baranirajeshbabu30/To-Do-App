532828240d48b7eca9a625b87ab0ce17c1140345
baranirajeshbabu30 <cheersbarani30@gmail.com>
2024-08-16 13:45:17 +0530

    api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://stellar-paprenjak-9c8f74.netlify.app/.netlify/functions', 
+  baseURL: 'https://stellar-paprenjak-9c8f74.netlify.app/.netlify/functions/api', 
   headers: {
     'Content-Type': 'application/json',
   },
